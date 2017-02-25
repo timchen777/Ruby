bad156ac140126695cb877e6679048313e65ac0f
Timchen7 <timchen7@yahoo.com>
2017-02-25 08:50:01 +0800

    u
    u

--- hw_quiz3.rb
+++ hw_quiz3.rb
@@ -9,7 +9,8 @@
             database: db/development.sqlite3
         (b) project/config/routes.rb: is used to control every single aspect of your URLs ( most of them using RESRful routing).
         Ex: get "users", to: "users#index"
-        (c) project/Gemfile：is a list of all gems that you want to include in the project. It is used with bundler (also a gem) to install, update, remove gems, related gems, and their version, bundle result will be loged in Gemfile.lock file。
+        (c) project/Gemfile：is a list of all gems that you want to include in the project. It is used with bundler (also a gem) 
+        to install, update, remove gems, related gems, and their version, bundle result will be loged in Gemfile.lock file。
 
     2.
     Q:MVC 架構裡的 M, V, 和 C 分別代表什麼？
@@ -17,7 +18,8 @@
     The MVC( Model, View, Controller) principle divides the application into three separate but closely cooperative subsystems.
     (a) M：Model, is implemented in ActiveRecord library, which provides an interface and binding between the tables in a relational database.
     (b) V：View, is triggered by a controller's decision to present the data.
-    (c) C：Controller, it directs traffic by querying the models for specific data, and organizing that data (searching, sorting, messaging it) into a form that fits the needs of a given view.
+    (c) C：Controller, it directs traffic by querying the models for specific data, and organizing that data (searching, sorting, 
+    messaging it) into a form that fits the needs of a given view.
 
     3.
     Q:請解釋 CRUD 是哪四個字的縮寫？
@@ -40,11 +42,13 @@
     5.
     Q:請解釋 model 檔案和 migration 檔案的差別？
     A:
-    (a) Model file：is the M in MVC , is the Active Record  which is the layer of the system responsible for representing business data and logic.
+    (a) Model file：is the M in MVC , is the Active Record  which is the layer of the system responsible for representing business data 
+    and logic.
     Ex: class User < ActiveRecord::Base
           has_many :post1s   
         end
-    (b) Migration file：is used by Active Record  to evolve your database schema over time (need to run rake db:migrate to create the table in database), instead of writing schema in pure SQL.
+    (b) Migration file：is used by Active Record  to evolve your database schema over time (need to run rake db:migrate to create the 
+    table in database), instead of writing schema in pure SQL.
     EX: class CreateUsers <ActiveRecord::Migration
           def change
             create_table :users do |t|
@@ -77,7 +81,8 @@
     8.
     Q:請解釋什麼是 ActiveRecord?
     A:
-    Active Record is the M(the model) in MVC, it is an implementation of the "Active Record pattern" which itself is a description of an Object Relational Mapping system.
+    Active Record is the M(the model) in MVC, it is an implementation of the "Active Record pattern" which itself is a description of an 
+    Object Relational Mapping system.
 
     9.
     Q:若今天需要為 Project 和 Issue 這兩個 Model 建立一對多的關係，請寫出實作上所需要的 migratiion 和 model 檔案
